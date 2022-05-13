6599b3ec817e1d0118df783093294379717ee338
nireo <eemil.lehtikangas1@gmail.com>
2022-05-13 22:56:30 +0300

    started adding parsing logic

--- jsonpp.h
+++ jsonpp.h
@@ -4,6 +4,7 @@
 #include <cctype>
 #include <cstddef>
 #include <memory>
+#include <optional>
 #include <string>        // for key names.
 #include <unordered_map> // for storing json values.
 #include <variant>
@@ -32,6 +33,12 @@ enum token_type_t {
   T_EOF,
 };
 
+enum status_t {
+  S_OK,
+  S_EXPECTED_OTHER,
+  S_FAIL,
+};
+
 class json_value_t;
 struct object_t {
   object_t();
@@ -53,6 +60,12 @@ struct token_t {
 };
 
 class json_value_t {
+  template <typename T>
+  json_value_t(json_type_t ty, T data) : type_(ty), value_(data) {}
+
+  template <typename T>
+  json_value_t(json_type_t ty, T &&data) : type_(ty), value_(std::move(data)) {}
+
 private:
   json_type_t type_{J_NONE};
   std::variant<object_t, std::string, int64_t, json_arr_t, std::monostate>
@@ -62,6 +75,24 @@ private:
 class json_t {
 public:
   json_t();
+
+  status_t parse(const std::string &p) {
+    auto tokens = parse_tokens(p);
+    size_t pos = 0;
+
+    while (tokens[pos].type_ != T_EOF) {
+      // in this loop objects can either be arrays or
+      // they can be objects.
+      if (tokens[pos].type_ == T_LEFT_BRACE) {
+
+      } else if (tokens[pos].type_ == T_LEFT_BRACKET) {
+      } else {
+        return S_FAIL; // unrecognized token at this point.
+      }
+    }
+
+    return S_OK;
+  }
 
 private:
   std::vector<token_t> parse_tokens(const std::string &p) {
@@ -140,7 +171,46 @@ private:
     return res;
   }
 
-  object_t root_obj_;
+  bool match(const std::vector<token_t> &tokens, size_t &pos, token_type_t ty) {
+    return tokens[pos].type_ == ty;
+  }
+
+  std::optional<object_t> parse_obj(const std::vector<token_t> &tokens,
+                                    size_t &pos) noexcept {
+    object_t res;
+    // objects have a string key, and then followed by a colon. After that we
+    // have either an object, an array, a number or a string.
+    ++pos; // skip the brace.
+
+    if (!match(tokens, pos, T_STRING)) {
+      return std::nullopt;
+    }
+    auto key = std::get<std::string>(tokens[pos].data_);
+    ++pos;
+    if (!match(tokens, pos, T_COLON)) {
+      return std::nullopt;
+    }
+    ++pos;
+
+    auto value = parse_json_value(tokens, pos);
+    if (!value.has_value()) {
+      return std::nullopt;
+    }
+    res[key] = std::move(value.value());
+
+    return res;
+  }
+
+  std::optional<json_value_t>
+  parse_json_value(const std::vector<token_t> &tokens, size_t &pos) noexcept {
+    if (tokens[pos].type_ == T_NUMBER) {
+      json_value_t val;
+    }
+
+    return std::nullopt;
+  }
+
+  json_value_t root_{};
 };
 
 } // namespace jsonpp
